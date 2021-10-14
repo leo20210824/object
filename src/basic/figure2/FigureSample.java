b24d4dc832b8e5ea2518726bcef4719719a951e6
leo20210824 <angler.reo@gmail.com>
2021-10-14 15:12:55 +0900

    20211014

---
+++ src/basic/figure2/FigureSample.java
@@ -0,0 +1,26 @@
+package basic.figure2;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//図形の面積
+class FigureSample {
+    public static void main(String[] args) {
+        //インスタンスの作成
+        //インスタンスの作成
+        Figure[] figures = {
+                new Rectangle(10,20),
+                new Triangle(20,15),
+                new Circle(10)
+        };
+
+
+        //出力
+        for (Figure figure:figures) {
+            //System.out.println(figure.getArea());
+            System.out.println(figure);
+        }
+    }
+}
