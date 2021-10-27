c9412c9e2d7fa0c0f1f04724f4a8eb7442825bd4
leo20210824 <angler.reo@gmail.com>
2021-10-27 15:18:27 +0900

    20211027変更

---
+++ src/training/training2/View.java
@@ -0,0 +1,19 @@
+package training.training2;
+
+
+
+public class View {
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 5;
+       int m = Math.max(a,b);
+        System.out.println(m);
+
+    String age = "31";
+    int n = Integer.parseInt(age);
+    System.out.println("あなたは来年、"+(n+1)+"歳になりますね。");
+
+
+}
+
+}
